0b7113ba0d79e60c4523f0947afd18d31cae8e5a
Kirill Light <kirill.gus@gmail.com>
2018-05-25 16:11:50 +0300

    Make output of results more efficient

--- test.js
+++ test.js
@@ -9,9 +9,12 @@ for (counter = 0; counter < 100; counter++) {
   testCases.forEach(([case1, case2]) => {
     let coefficient = diceCoefficient(case1, case2)
     if (coefficient > 0) {
-      results.push(coefficient)
+      results.push({
+        coefficient: coefficient,
+        strings: [case1, case2]
+      })
     }
   })
 }
 
-console.log(results.filter(el => el > 0.5))
+console.log(results.filter(el => el.coefficient > 0.5))
